20e41f4157f0df793af1650bbefd693f57791956
willicom2000 <willicom2000_9@hotmail.com>
2026-09-07 10:04:50 -0300

    Update videoid.js

--- 2.11.0/example-videoid/www/js/videoid.js
+++ 2.11.0/example-videoid/www/js/videoid.js
@@ -7,30 +7,57 @@ function callVideoId()
         return;
     }
 
-    console.log('callVideoId started...');
-    $("#messageResult").html("Starting proccess...").addClass("blink").css("color", "#000000").css("text-align","center").show();
-
     if (isStartingSDK) {
         console.log("A process is running...");
         return false;
     }
     isStartingSDK = true;
 
-    var config = new SdkVideoIdConfig();
-    config.setMode(facephi.plugins.sdkvideoid.mode.SdkVideoIdMode.face_document_front)
-    config.setSectionTime(5000);
-    config.setShowTutorial(true);
+    console.log('callVideoId started...');
+    $("#messageResult").html("Starting proccess...").addClass("blink").css("color", "#000000").css("text-align","center").show();
 
-    videoIdResponse = null;
-    facephi.plugins.sdkvideoid.launchVideoId(config)
-    .then(
-        (result) => console.log(result),
-        (err) => console.log(err),
-    )
-    .finally (() =>
-    {
+    try {
+        var config = new SdkVideoIdConfig();
+        config.setMode(facephi.plugins.sdkvideoid.mode.SdkVideoIdMode.face_document_front)
+        config.setSectionTime(5000);
+        config.setShowTutorial(true);
+
+        videoIdResponse = null;
+        facephi.plugins.sdkvideoid.launchVideoId(config)
+        .then(
+            (result) =>
+            {
+                console.log(result);
+                if (result == null || result == undefined) {
+                    showErrorUI(fphi_str_unknown_error);
+                    return;
+                }
+
+                switch (parseInt(result['finishStatus']))
+                {
+                    case SdkMobileFinishStatus.Ok:
+                        videoIdResponse = result;
+                        $("#messageResult").html("").removeClass("blink").hide();
+                        break;
+
+                    case SdkMobileFinishStatus.Error:
+                        showErrorUI(result['errorType'] || fphi_str_unknown_error);
+                        break;
+
+                    default:
+                        showErrorUI(fphi_str_unknown_error);
+                }
+            },
+            (err) => showErrorUI(err),
+        )
+        .finally (() =>
+        {
+            isStartingSDK = false;
+            console.log("callVideoId finished...");
+        });
+    } catch (e) {
         isStartingSDK = false;
-        console.log("callVideoId finished...");
-        $("#messageResult").html("").removeClass("blink").css("color", "#ff0000").css("text-align", "center").show();
-    });
+        console.log(e);
+        showErrorUI(e.message || e);
+    }
 }
