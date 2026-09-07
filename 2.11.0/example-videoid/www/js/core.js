7d9a3b4018301a83a6e0e8ed6de9d559e733a24d
willicom2000 <willicom2000_9@hotmail.com>
2026-09-07 09:20:00 -0300

    Update core.js

--- 2.11.0/example-videoid/www/js/core.js
+++ 2.11.0/example-videoid/www/js/core.js
@@ -28,14 +28,6 @@ function callInitSession()
     }
 
     console.log('callInitSession started...');
-    $("#messageResult").html("Starting proccess...").addClass("blink").css("color", "#000000").css("text-align","center").show();
-
-    if (isStartingSDK) {
-        console.log("A process is running...");
-        return false;
-    }
-    isStartingSDK = true;
-
     const lic       = window.cordova.platformId.toUpperCase() == "IOS" ? LICENSEIOS_NEW : LICENSEANDROID_NEW
     const apiKey    = window.cordova.platformId.toUpperCase() == "IOS" ? LICENSE_APIKEY_IOS : LICENSE_APIKEY_ANDROID
     facephi.plugins.sdkcore.launchInitSession({
